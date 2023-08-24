822e08a997e598dda2c8a96e014c59a4d76266ed
Tretiak Serhii <tretyak8936@gmail.com>
2023-08-24 23:30:24 +0300

    added style for asideBar

--- components/AsideBar/AsideBar.tsx
+++ components/AsideBar/AsideBar.tsx
@@ -1,28 +1,45 @@
 import Link from 'next/link';
 import React from 'react';
+import avatar from '../../public/avatar.jpeg';
+import settings from '../../public/settings.png';
+import classes from '../../styles/asidebar.module.scss';
+// import cn from 'classnames';
 
 export const AsideBar: React.FC = () => {
   return (
-    <div className="aside-bar">
-      <nav className="nav">
-        <div>
-          <Link href={'/orders'}>ORDERS</Link>
+    <div className={classes.aside_bar}>
+      <div className={classes.aside_bar__user_info}>
+        <img src={avatar.src} alt="user avatar" />
+        <img
+          className={classes.settings_icon}
+          src={settings.src}
+          alt="settings icon"
+        />
+      </div>
+
+      <nav className={classes.nav}>
+        <div
+          className={classes.nav__link}
+          // className={({ isActive }) =>
+          //   cn(classes.nav__link, { [classes.nav__link__active]: isActive })}
+        >
+          <Link href={'/orders'}>Orders</Link>
         </div>
 
-        <div>
-          <Link href={'/groups'}>GROUPS</Link>
+        <div className={classes.nav__link}>
+          <Link href={'/groups'}>Groups</Link>
         </div>
 
-        <div>
-          <Link href={'/products'}>PRODUCTS</Link>
+        <div className={classes.nav__link}>
+          <Link href={'/products'}>Products</Link>
         </div>
 
-        <div>
-          <Link href={'/users'}>USERS</Link>
+        <div className={classes.nav__link}>
+          <Link href={'/users'}>Users</Link>
         </div>
 
-        <div>
-          <Link href={'/settings'}>SETTINGS</Link>
+        <div className={classes.nav__link}>
+          <Link href={'/settings'}>Settings</Link>
         </div>
       </nav>
     </div>
