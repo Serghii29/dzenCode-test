04bbfb874a2dd3b491d9fe61fa5ed465810f1031
Tretiak Serhii <tretyak8936@gmail.com>
2023-08-27 08:44:59 +0300

    wrote logic for search bar

--- components/ProductsList/ProductsList.tsx
+++ components/ProductsList/ProductsList.tsx
@@ -2,13 +2,20 @@ import { useAppSelector } from '@/store/hooks';
 import React from 'react';
 import { ProductItem } from '../ProductItem/ProductItem';
 import classes from '../../styles/products_list.module.scss';
+import { useRouter } from 'next/router';
+import { searchedProducts } from '@/utils/searched';
 
 export const ProductsList: React.FC = () => {
   const { products } = useAppSelector((state) => state.products);
 
+  const { query } = useRouter();
+  const searchParams = query.searchParams;
+
+  const preparedOrders = searchedProducts(products, searchParams as string);
+
   return (
     <div className={classes.products_list}>
-      {products.map((product) => (
+      {preparedOrders.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
     </div>
